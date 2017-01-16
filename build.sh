76a1e6a77597b97f41f407682961a762d4b713f3
Michael <Alexander>
2017-01-16 09:05:51 +0000

    Adding build.sh; updating figure paths.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git checkout master
+git pull origin master
+git checkout gh-pages
+(make > stdout-make 2> stderr make \
+	&& git commit -a -m "Build." \
+	&& git push origin gh-pages) || \
+    (echo 'make failed, stderr:' && cat stderr-make)
+git checkout master
