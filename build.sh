cc1947980cf29e7b0c5dcfa61a11efd6fd974a7e
Michael <Alexander>
2017-01-16 13:40:07 +0000

    Fix build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 git checkout master
 git pull origin master
 git checkout gh-pages
+git merge -m "Merge master" master
 (make > stdout-make 2> stderr-make \
 	&& git commit -a -m "Build." \
 	&& git push origin gh-pages) || \
