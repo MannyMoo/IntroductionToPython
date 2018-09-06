9d631016432d750e4a37e384e2ab656fd9332a9a
MannyMoo <airstream.driver@gmail.com>
2018-09-06 18:40:27 +0200

    Finish crash course.

---
+++ maketoc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+titles=$(grep -A 4 '"cell_type": "markdown"' $1 | grep '"#')
+python -c "titles = '''${titles}'''
+for line in titles.splitlines() :
+    line = line.replace('\"', '')
+    splitline = line.split()
+    n = len(splitline[0])-1
+    print ' ' * max(4 * n - 1, 0), '-', '[{0}](#{1})'.format(' '.join(splitline[1:]), '-'.join(splitline[1:]))
+"
