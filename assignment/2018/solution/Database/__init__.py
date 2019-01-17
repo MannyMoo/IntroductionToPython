65d4c025b0a82d47a7f9ac5b72dd900ddce8efd1
MannyMoo <airstream.driver@gmail.com>
2019-01-17 21:34:20 +0000

    Remove past assignments.

--- assignment/2018/solution/Database/__init__.py
+++
@@ -1,7 +0,0 @@
-'''Package for database reading and statistical analysis.'''
-
-# So you can do
-# from Database import Database
-# rather than
-# from Database.DB import Database
-from DB import Database
